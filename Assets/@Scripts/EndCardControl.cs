1c6ee0c03ddcef0777bb399e43c5fa8784a5e76c
dag <rubtub2314@gmail.com>
2025-07-08 05:38:51 +0900

    엔드 카드 추가

--- Assets/@Scripts/EndCardControl.cs
+++ Assets/@Scripts/EndCardControl.cs
@@ -1,17 +1,35 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// End Card를 제어하는 클래스
 /// </summary>
 public class EndCardControl : MonoBehaviour
 {
+    [SerializeField] private TMP_Text playNowTmp;
+    [SerializeField] private TMP_Text restartTmp;
+    [SerializeField] private GameObject restartButtonObject;
+
+    [LunaPlaygroundField(fieldTitle:"Play Now Button's Text")]
+    public string playNowText;
+    [LunaPlaygroundField(fieldTitle: "Restart Button's Text")]
+    public string restartText;
+    [LunaPlaygroundField(fieldTitle: "Download Page Link")]
+    public string downloadPageUrl;
+
     /// <summary>
     /// 엔드카드를 노출시키는 함수
     /// </summary>
     /// <param name="canRestart">true일 때, 다운로드 버튼과 함께 다시 시작도 제공함.</param>
     public void ShowEndCard(bool canRestart)
     {
+        this.gameObject.SetActive(true);
 
+        playNowTmp.text = playNowText;
+        restartTmp.text = restartText;
+
+        restartButtonObject.SetActive(canRestart);
     }
 
     /// <summary>
@@ -19,7 +37,9 @@ public class EndCardControl : MonoBehaviour
     /// </summary>
     public void RestartButtonClickEventHandler()
     {
+        this.gameObject.SetActive(false);
 
+        GameFlowControl.Instance.InitalizeGame();
     }
 
     /// <summary>
@@ -27,6 +47,6 @@ public class EndCardControl : MonoBehaviour
     /// </summary>
     public void DownloadButtonClickEventHandler()
     {
-
+        Application.OpenURL(downloadPageUrl);
     }
 }
